811d12432b29045b09078aed35909fae1267592e
vedat <vedatsedir@hotmail.com>
2021-02-07 21:04:20 +0300

    emlak projesi tamamlandi

---
+++ Emlak.DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Emlak.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Emlak.DAL.EmlakContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Emlak.DAL.EmlakContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
